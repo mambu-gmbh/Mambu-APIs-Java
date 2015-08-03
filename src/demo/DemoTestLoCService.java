1739fb1fecdf0485bebcf3c3f38c958893d70e29
Michael Danilkis <mikedan7@gmail.com>
2015-08-03 16:27:21 -0700

    Add APIs to add and remove account to Lines
    1. Added add loan account and savings account to line of credit. See
    MBU-9864
    2. Added remove an account form line of credit.  See MBU-9873
    3. Update demo tests

--- src/demo/DemoTestLoCService.java
+++ src/demo/DemoTestLoCService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.mambu.apisdk.MambuAPIFactory;
 import com.mambu.apisdk.exception.MambuApiException;
 import com.mambu.apisdk.services.LinesOfCreditService;
-import com.mambu.apisdk.util.MambuEntityType;
 import com.mambu.linesofcredit.shared.model.AccountsFromLineOfCredit;
 import com.mambu.linesofcredit.shared.model.LineOfCredit;
 import com.mambu.loans.shared.model.LoanAccount;
@@ -27,9 +26,13 @@ public class DemoTestLoCService {
 
 			testGetLinesOfCredit(); // Available since 3.11
 
-			String lineOfCrediId = testGetCustomerLinesOfCredit(); // Available since 3.11
+			testGetCustomerLinesOfCredit(); // Available since 3.11
 
-			testGetAccountsForLineOfCredit(lineOfCrediId);// Available since 3.11
+			// test get accounts for LoC
+			AccountsFromLineOfCredit locAccounts = testGetAccountsForLineOfCredit();// Available since 3.11
+
+			// test add and remove accounts from LoC.
+			testAddAndRemoveAccountsForLineOfCredit(locAccounts);// Available since 3.12.2
 
 		} catch (MambuApiException e) {
 			System.out.println("Exception caught in Demo Test Lines of Credit Service");
@@ -73,7 +76,7 @@ public class DemoTestLoCService {
 	 * 
 	 * @return any of the LoC IDs
 	 */
-	public static String testGetCustomerLinesOfCredit() throws MambuApiException {
+	public static void testGetCustomerLinesOfCredit() throws MambuApiException {
 		System.out.println("\nIn testGetCustomerLinesOfCredit");
 
 		LinesOfCreditService linesOfCreditService = MambuAPIFactory.getLineOfCreditService();
@@ -81,26 +84,19 @@ public class DemoTestLoCService {
 		Integer limit = 30;
 		// Test Get line of credit for a Client
 		// Get Demo Client ID first
-		DemoEntityParams entityParams = DemoEntityParams.getEntityParams(MambuEntityType.CLIENT);
-		final String clientId = entityParams.getId();
+
+		final String clientId = DemoUtil.getDemoClient().getId();
 		// Get Lines of Credit for a client
 		List<LineOfCredit> clientLoCs = linesOfCreditService.getClientLinesOfCredit(clientId, offset, limit);
-		System.out.println(clientLoCs.size() + " lines of credit  for Client " + entityParams.getName() + " "
-				+ entityParams.getId());
+		System.out.println(clientLoCs.size() + " lines of credit  for Client " + clientId);
 
-		String clientLoCId = (clientLoCs.size() > 0) ? clientLoCs.get(0).getId() : null;
 		// Test Get line of credit for a Group
 		// Get Demo Group ID first
-		entityParams = DemoEntityParams.getEntityParams(MambuEntityType.GROUP);
-		final String groupId = entityParams.getId();
+		final String groupId = DemoUtil.getDemoGroup().getId();
 		// Get Lines of Credit for a group
 		List<LineOfCredit> groupLoCs = linesOfCreditService.getGroupLinesOfCredit(groupId, offset, limit);
-		System.out.println(groupLoCs.size() + " lines of credit for Group " + entityParams.getName() + " "
-				+ entityParams.getId());
-		String groupLoCId = (groupLoCs.size() > 0) ? groupLoCs.get(0).getId() : null;
+		System.out.println(groupLoCs.size() + " lines of credit for Group " + groupId);
 
-		// return the Id for one of the LoC for subsequent tests
-		return (clientLoCId != null) ? clientLoCId : groupLoCId;
 	}
 
 	/**
@@ -108,15 +104,17 @@ public class DemoTestLoCService {
 	 * 
 	 * @param lineOfCreditId
 	 *            an id or encoded key for a Line of Credit
+	 * @return accounts for a line of credit
 	 * @throws MambuApiException
 	 */
-	public static void testGetAccountsForLineOfCredit(String lineOfCreditId) throws MambuApiException {
+	public static AccountsFromLineOfCredit testGetAccountsForLineOfCredit() throws MambuApiException {
 		System.out.println("\nIn testGetAccountsForLineOfCredit");
 		// Test Get Accounts for a line of credit
 
+		String lineOfCreditId = DemoUtil.demoLineOfCreditId;
 		if (lineOfCreditId == null) {
-			System.out.println("No Line of credit to get accounts");
-			return;
+			System.out.println("WARNING: No Demo Line of credit defined");
+			return null;
 		}
 
 		LinesOfCreditService linesOfCreditService = MambuAPIFactory.getLineOfCreditService();
@@ -128,5 +126,89 @@ public class DemoTestLoCService {
 		List<SavingsAccount> savingsAccounts = accountsForLoC.getSavingsAccounts();
 		System.out.println("Total Loan Accounts=" + loanAccounts.size() + "\tTotal Savings Accounts="
 				+ savingsAccounts.size() + " for LoC=" + lineOfCreditId);
+
+		return accountsForLoC;
+	}
+
+	// Test deleting and adding accounts to a Line of Credit
+	public static void testAddAndRemoveAccountsForLineOfCredit(AccountsFromLineOfCredit accountsForLoC)
+			throws MambuApiException {
+		System.out.println("\nIn testAddAndRemoveAccountsForLineOfCredit");
+
+		String lineOfCreditId = DemoUtil.demoLineOfCreditId;
+		if (lineOfCreditId == null) {
+			System.out.println("WARNING: No Demo Line of credit defined");
+			return;
+		}
+
+		// Test remove and add for Loan Accounts
+		List<LoanAccount> loanAccounts = accountsForLoC.getLoanAccounts();
+		testdeleteAndAddLoanAccounts(lineOfCreditId, loanAccounts);
+
+		// Test remove and add for Savings Accounts
+		List<SavingsAccount> savingsAccounts = accountsForLoC.getSavingsAccounts();
+		testdeleteAndAddSavingsAccounts(lineOfCreditId, savingsAccounts);
+	}
+
+	// For each Loan account associated with a credit line test deleting and adding it back
+	private static void testdeleteAndAddLoanAccounts(String lineOfCreditId, List<LoanAccount> accounts)
+			throws MambuApiException {
+		System.out.println("\nIn testdeleteAndAddLoanAccounts for LoC=" + lineOfCreditId);
+
+		LinesOfCreditService linesOfCreditService = MambuAPIFactory.getLineOfCreditService();
+		String accountId = null;
+		if (accounts == null || accounts.size() == 0) {
+			System.out.println("WARNING: no Loan Account to remove for LoC=" + lineOfCreditId);
+		} else {
+			// We have assigned accounts. Test remove and then test adding it back
+			// Test removing Loan Account. Accounts requiring LoC cannot be removed, so try until can
+			for (LoanAccount account : accounts) {
+				accountId = account.getId();
+				try {
+					boolean deleted = linesOfCreditService.deleteLoanAccount(lineOfCreditId, accountId);
+					System.out.println("Removed Status=" + deleted + "\tAccount with ID=" + accountId
+							+ " deleted from LoC=" + lineOfCreditId);
+					// Deleted OK, now add the same back
+					LoanAccount addedLoan = linesOfCreditService.addLoanAccount(lineOfCreditId, accountId);
+					System.out.println("Added Loan Account with ID=" + addedLoan.getId() + " to LoC=" + lineOfCreditId);
+				} catch (MambuApiException e) {
+					System.out.println("Failed to remove account " + accountId + "\tMessage=" + e.getErrorMessage());
+				}
+
+			}
+
+		}
+	}
+
+	// For each Savings account associated with a credit line test deleting and adding it back
+	private static void testdeleteAndAddSavingsAccounts(String lineOfCreditId, List<SavingsAccount> accounts)
+			throws MambuApiException {
+		System.out.println("\nIn testdeleteAndAddSavingsAccounts for LoC=" + lineOfCreditId);
+
+		LinesOfCreditService linesOfCreditService = MambuAPIFactory.getLineOfCreditService();
+		String accountId = null;
+		if (accounts == null || accounts.size() == 0) {
+			System.out.println("WARNING: no Savings Account to remove for LoC=" + lineOfCreditId);
+		} else {
+			// We have assigned accounts. Test remove and then test adding it back
+			// Test removing Savings Account. Accounts requiring LoC cannot be removed, so try until can
+			for (SavingsAccount account : accounts) {
+				accountId = account.getId();
+				try {
+					boolean deleted = linesOfCreditService.deleteSavingsAccount(lineOfCreditId, accountId);
+					System.out.println("Removed Status=" + deleted + "\tAccount with ID=" + accountId
+							+ " deleted from LoC=" + lineOfCreditId);
+					;
+					// Deleted OK, now add the same back
+					SavingsAccount addedAccount = linesOfCreditService.addSavingsAccount(lineOfCreditId, accountId);
+					System.out.println("Added Savings Account with ID=" + addedAccount.getId() + " to LoC="
+							+ lineOfCreditId);
+				} catch (MambuApiException e) {
+					System.out.println("Failed to remove account " + accountId + "\tMessage=" + e.getErrorMessage());
+				}
+
+			}
+
+		}
 	}
 }
