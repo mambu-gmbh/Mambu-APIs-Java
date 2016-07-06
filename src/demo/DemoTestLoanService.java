c2b772dd47d965f5342a63fa904a3a436049f54c
Florin Parascan <florinparascan@Florins-iMac.local>
2016-07-06 13:37:56 +0300

    Remove the loan account creation and approval from the testEditLoanAmountForActiveRevolvingCreditLoans and add loan product type and account state checking to the method

--- src/demo/DemoTestLoanService.java
+++ src/demo/DemoTestLoanService.java
@@ -140,10 +140,6 @@ public class DemoTestLoanService {
 				}
 
 				try {
-					if (productType.equals(LoanProductType.REVOLVING_CREDIT)) {
-						// Edit the loan amount for active revolving credit loan. See MBU-12661
-						testEditLoanAmountForActiveRevolvingCreditLoans();
-					}
 
 					// Create account to test patch, approve, undo approve, reject, close
 					testCreateJsonAccount();
@@ -171,6 +167,8 @@ public class DemoTestLoanService {
 
 					// Test Disburse and Undo disburse
 					testDisburseLoanAccount();
+					// Edit the loan amount only for active revolving credit loans
+					testEditLoanAmountForActiveRevolvingCreditLoans();
 					testUndoDisburseLoanAccount(); // Available since 3.9
 					testDisburseLoanAccount();
 					testGetLoanAccountTransactions();
@@ -250,31 +248,42 @@ public class DemoTestLoanService {
 
 	}
 
+	// Edit the loan amount for active revolving credit loans. See MBU-12661
 	private static void testEditLoanAmountForActiveRevolvingCreditLoans() throws MambuApiException {
 
-		testCreateJsonAccount();
+		System.out.println(methodName = "\nIn testEditLoanAmountForActiveRevolvingCreditLoans");
+
+		LoanProductType loanProductType = demoProduct.getLoanProductType();
 
 		LoansService loanService = MambuAPIFactory.getLoanService();
 
-		// Change loan account state to APPROVED
-		loanService.approveLoanAccount(NEW_LOAN_ACCOUNT_ID, "");
+		// Get the updated state of the loan account
+		AccountState accountState = loanService.getLoanAccount(NEW_LOAN_ACCOUNT_ID).getAccountState();
 
-		loanService.disburseLoanAccount(NEW_LOAN_ACCOUNT_ID, newAccount.getLoanAmount(), null, "");
+		// Edit the loan amount for active revolving credit loan
+		if (loanProductType.equals(LoanProductType.REVOLVING_CREDIT) && accountState.equals(AccountState.ACTIVE)) {
 
-		// Due to the fact that the PATCH operation for loan accounts accepts only the loan amount as request body, the
-		// unnecessary default values for a loan account should be updated to null. The newly created loan amount object
-		// should have values only for the id and loanAmount fields
-		LoanAccount newLoanAccount = new LoanAccount();
-		newLoanAccount.setId(NEW_LOAN_ACCOUNT_ID);
-		newLoanAccount.setLoanAmount(newAccount.getLoanAmount().add(new BigDecimal("55")));
-		newLoanAccount.setPeriodicPayment((BigDecimal) null);
-		newLoanAccount.setRepaymentInstallments(null);
-		newLoanAccount.setGracePeriod(null);
-		newLoanAccount.setPrincipalRepaymentInterval(null);
-		newLoanAccount.setArrearsTolerancePeriod(null);
+			// Due to the fact that the PATCH operation for loan accounts accepts only the loan amount as request body,
+			// the unnecessary default values for a loan account should be updated to null. The newly created loan
+			// account object should have values only for the id and loanAmount fields
+			LoanAccount newLoanAccount = new LoanAccount();
+			newLoanAccount.setId(NEW_LOAN_ACCOUNT_ID);
+			newLoanAccount.setLoanAmount(newAccount.getLoanAmount().add(new BigDecimal("55")));
+			newLoanAccount.setPeriodicPayment((BigDecimal) null);
+			newLoanAccount.setRepaymentInstallments(null);
+			newLoanAccount.setGracePeriod(null);
+			newLoanAccount.setPrincipalRepaymentInterval(null);
+			newLoanAccount.setArrearsTolerancePeriod(null);
 
-		// Edit the loan amount
-		loanService.patchLoanAccount(newLoanAccount);
+			// Edit the loan amount
+			boolean patchResult = loanService.patchLoanAccount(newLoanAccount);
+
+			System.out.println("The loan amount was edited for the loan account " + NEW_LOAN_ACCOUNT_ID + ". Status: "
+					+ patchResult);
+		} else {
+			System.out.println(
+					"The loan amount does not meet the prerequisites, therefore its loan amount will not be updated.");
+		}
 	}
 
 	public static void testGetLoanAccount() throws MambuApiException {
