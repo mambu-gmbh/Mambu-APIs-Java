4d82381f0bcd34c7c4e5f80e9a1797c2f86cdb8c
mmatecki <ZAQ12wsx>
2016-07-18 14:56:43 +0200

    Now you can use domainname with port and switch off https

--- src/com/mambu/apisdk/util/URLHelper.java
+++ src/com/mambu/apisdk/util/URLHelper.java
@@ -49,7 +49,7 @@ public class URLHelper {
 		String encodedUrl;
 		try {
 
-			URI uri = new URI(WEB_PROTOCOL, domainName, API_ENDPOINT + details, null);
+			URI uri = createURI(details);
 			encodedUrl = uri.toString();
 
 			return encodedUrl;
@@ -134,5 +134,28 @@ public class URLHelper {
 		params.remove(APIData.LIMIT);
 
 		return urlWithParams;
+	}
+
+	private URI createURI(String details) throws URISyntaxException {
+		if(isDomainNameWithPort()) {
+			return new URI(WEB_PROTOCOL, null, host(), port(), API_ENDPOINT + details, null, null);
+		}
+		return new URI(WEB_PROTOCOL, domainName, API_ENDPOINT + details, null);
+	}
+
+	private String host() {
+		return this.domainName.split(":")[0];
+	}
+
+	private int port() {
+		return Integer.parseInt(this.domainName.split(":")[1]);
+	}
+
+	private boolean isDomainNameWithPort() {
+		return this.domainName.matches(".*:[0-9]*");
+	}
+
+	public static void useHttp() {
+		URLHelper.WEB_PROTOCOL = "http";
 	}
 }
