98e11e30b72fe0416311ccdcbc00576ef03a194b
Andrei Costros <andrei.costros@mambu.com>
2017-01-31 18:07:48 +0200

    Added new public method handling resubmission of failed notification messages

--- src/demo/DemoTestClientService.java
+++ src/demo/DemoTestClientService.java
@@ -10,8 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
-import org.mockito.internal.util.reflection.Whitebox;
-
 import com.mambu.accounts.shared.model.AccountHolderType;
 import com.mambu.api.server.handler.documents.model.JSONDocument;
 import com.mambu.apisdk.MambuAPIFactory;
